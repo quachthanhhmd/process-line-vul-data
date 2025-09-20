b0cf01c82e77a38106b4d3c78f490a700cec7075
seokjeon <33389418+seokjeon@users.noreply.github.com>
2025-09-20 21:41:26 +0900

    fix: align path not with docker based processing but gpu server

--- Experiments/DeepWukong/deepwukong_pipeline.sh
+++ Experiments/DeepWukong/deepwukong_pipeline.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
 project_name="all"
-SLURM_TMPDIR="/code/models/DeepWukong/data/realvul"
-cd $SLURM_TMPDIR
+SLURM_TMPDIR="data/${project_name}"
+# cd $SLURM_TMPDIR
 
 #source_code extraction
-tar -xf "/data/dataset/${project_name}_source_code.tar.xz" -C $SLURM_TMPDIR
-mv "SLURM_TMPDIR/${project_name}_source_code" "SLURM_TMPDIR/source_code"
+mv "../../Dataset/all_source_code/*" "${SLURM_TMPDIR}/source_code/*" # TODO: 다른 SW 테스트 할때는 tar -xf "Dataset/${project_name}_source_code.tar.gz" -C $SLURM_TMPDIR
 
 #sGeneration of PDG
-/tools/ReVeal/code-slicer/joern/joern-parse source_code
-mv parsed csv
-tar -I 'pigz -p $(nproc) -6' -cf "/data/dataset/${project_name}_csv.tar.gz" csv # tar -zcvf "/data/dataset/${project_name}_csv.tar.gz" csv
+/data/ReVeal/code-slicer/joern/joern-parse "${SLURM_TMPDIR}/source_code" # TODO: joern-parse 경로 수정 필요
+
+mv parsed "${SLURM_TMPDIR}/csv"
+tar -I 'pigz -p $(nproc) -6' -cf "../../Dataset/${project_name}_csv.tar.gz" csv # tar -zcvf "Dataset/${project_name}_csv.tar.gz" csv
 
 #Generation of XFG
-python "/code/models/DeepWukong/data_generator.py" -c "/config/config.yaml"
-cd $SLURM_TMPDIR
-tar -I 'pigz -p $(nproc) -6' -cf "/data/dataset/XFG_${project_name}.tar.gz" XFG # 원래 이건데 왼쪽으로 해봄 tar -zcf "/data/dataset/XFG_${project_name}.tar.gz" XFG
+python "data_generator.py" -c "./config/config.yaml"
+tar -I 'pigz -p $(nproc) -6' -cf "../../Dataset/XFG_${project_name}.tar.gz" XFG # 원래 이건데 왼쪽으로 해봄 tar -zcf "/data/dataset/XFG_${project_name}.tar.gz" XFG
 
 #Symbolize and Split Dataset
-python "/code/models/DeepWukong/preprocess/dataset_generator.py" -c "/config/config.yaml"
+python "preprocess/dataset_generator.py" -c "/config/config.yaml"
 cd $SLURM_TMPDIR
-tar -I 'pigz -p $(nproc) -6' -cf "/data/dataset/XFG_${project_name}__processed.tar.gz" XFG # tar -zcf "/data/dataset/XFG_${project_name}__processed.tar.gz" XFG
+tar -I 'pigz -p $(nproc) -6' -cf "../../Dataset/XFG_${project_name}__processed.tar.gz" XFG # tar -zcf "/data/dataset/XFG_${project_name}__processed.tar.gz" XFG
 
 #Word Embedding Pretraining
-python "/code/models/DeepWukong/preprocess/word_embedding.py" -c "/config/config.yaml"
+python "preprocess/word_embedding.py" -c "./config/config.yaml"
 
 #Training and Testing
-python "/code/models/DeepWukong/run.py" -c "/config/config.yaml"
+python "run.py" -c "./config/config.yaml"
 
